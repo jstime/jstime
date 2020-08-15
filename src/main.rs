abf868603e19d3dec542f40db380ae663f680011
Myles Borins <myles.borins@gmail.com>
2020-08-15 01:30:40 -0400

    hello rusty v8

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use rusty_v8 as v8;
 
 fn main() {
-  let stdout = stdout();
-  let message = String::from("Hello fellow Rustaceans!");
-  let width = message.chars().count();
-  
-  let mut writer = BufWriter::new(stdout.lock());
-  say(message.as_bytes(), width, &mut writer).unwrap();
+  let platform = v8::new_default_platform().unwrap();
+  v8::V8::initialize_platform(platform);
+  v8::V8::initialize();
+
+  let isolate = &mut v8::Isolate::new(Default::default());
+
+  let scope = &mut v8::HandleScope::new(isolate);
+  let context = v8::Context::new(scope);
+  let scope = &mut v8::ContextScope::new(scope, context);
+
+  let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
+  println!("javascript code: {}", code.to_rust_string_lossy(scope));
+
+  let mut script = v8::Script::compile(scope, code, None).unwrap();
+  let result = script.run(scope).unwrap();
+  let result = result.to_string(scope).unwrap();
+  println!("result: {}", result.to_rust_string_lossy(scope));
 }
