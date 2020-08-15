f49d8d643eec38098430a2bb825d830bed136c21
Myles Borins <myles.borins@gmail.com>
2020-08-15 01:17:24 -0400

    hello intensifies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+  let stdout = stdout();
+  let message = String::from("Hello fellow Rustaceans!");
+  let width = message.chars().count();
+  
+  let mut writer = BufWriter::new(stdout.lock());
+  say(message.as_bytes(), width, &mut writer).unwrap();
 }
