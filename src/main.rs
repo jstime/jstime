8ee35b37ac2b3ad3d89e2eebe08bfa26f8002778
Myles Borins <myles.borins@gmail.com>
2020-08-16 00:06:29 -0400

    refactor repl

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::process;
 
 use rusty_v8 as v8;
 
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
+mod repl;
 
 fn run(isolate: &mut v8::Isolate, js: &str) -> String {
   let scope = &mut v8::HandleScope::new(isolate);
@@ -38,42 +37,6 @@ fn run_file(filepath: &str) {
   }
 }
 
-fn repl() {
-  let isolate = &mut v8::Isolate::new(Default::default());
-  let scope = &mut v8::HandleScope::new(isolate);
-  let context = v8::Context::new(scope);
-  let scope = &mut v8::ContextScope::new(scope, context);
-
-  let mut rl = Editor::<()>::new();
-  
-  println!("\nWelcome to jstime!\n");
-  loop {
-    let readline = rl.readline(">> ");
-    match readline {
-      Ok(line) => {
-        let code = v8::String::new(scope, &line).unwrap();
-
-        let script = v8::Script::compile(scope, code, None).unwrap();
-        let result = script.run(scope).unwrap();
-        let result = result.to_string(scope).unwrap();
-        println!("{}", &result.to_rust_string_lossy(scope));
-      },
-      Err(ReadlineError::Interrupted) => {
-        println!("Thanks for stopping by!");
-        break
-      },
-      Err(ReadlineError::Eof) => {
-        println!("Eof'd");
-        break
-      },
-      Err(err) => {
-        println!("Error: {:?}", err);
-        break
-      }
-    }
-  }
-}
-
 fn main() {
   let args: Vec<String> = env::args().collect();
   let len = args.len();
@@ -88,7 +51,7 @@ fn main() {
   v8::V8::initialize();
   
   match len {
-    1 => repl(),
+    1 => repl::start(),
     2 => run_file(&args[1]),
     _ => println!("Woopsie Doodles")
   }
---
+++ src/repl.rs
@@ -0,0 +1,40 @@
+use rusty_v8 as v8;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+pub fn start() {
+  let isolate = &mut v8::Isolate::new(Default::default());
+  let scope = &mut v8::HandleScope::new(isolate);
+  let context = v8::Context::new(scope);
+  let scope = &mut v8::ContextScope::new(scope, context);
+
+  let mut rl = Editor::<()>::new();
+
+  println!("\nWelcome to jstime!\n");
+  loop {
+    let readline = rl.readline(">> ");
+    match readline {
+      Ok(line) => {
+        let code = v8::String::new(scope, &line).unwrap();
+
+        let script = v8::Script::compile(scope, code, None).unwrap();
+        let result = script.run(scope).unwrap();
+        let result = result.to_string(scope).unwrap();
+        println!("{}", &result.to_rust_string_lossy(scope));
+      },
+      Err(ReadlineError::Interrupted) => {
+        println!("Thanks for stopping by!");
+        break
+      },
+      Err(ReadlineError::Eof) => {
+        println!("Eof'd");
+        break
+      },
+      Err(err) => {
+        println!("Error: {:?}", err);
+        break
+      }
+    }
+  }
+}
