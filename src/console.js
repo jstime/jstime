25956bbc478383325328d002eca00fea1fae5674
Myles Borins <myles.borins@gmail.com>
2020-08-19 03:58:25 -0400

    src: more or less spec compliant now

--- src/console.js
+++ src/console.js
@@ -1,42 +1,43 @@
 // Cosole
 // https://console.spec.whatwg.org/#console-namespace
-// Logging
-// void assert(optional boolean condition = false, any... data);
-// void clear();
-// void debug(any... data);
-// void error(any... data);
-// void info(any... data);
-// void log(any... data);
-// void table(optional any tabularData, optional sequence<DOMString> properties);
-// void trace(any... data);
-// void warn(any... data);
-// void dir(optional any item, optional object? options);
-// void dirxml(any... data);
 
-// log(...data)
-// Perform Logger("log", data).
+const internalSymbol = Symbol('jstime');
 
-// void error(any... data);
-// Perform Logger("error", data).
+globalThis[internalSymbol] = {};
 
-// Logger(logLevel, args)
-// https://console.spec.whatwg.org/#Logger
-// 1.  If args is empty, return.
-//
-// 2. Let first be args[0].
-//
-// 3. Let rest be all elements following first in args.
-//
-// 4. If rest is empty, perform Printer(logLevel, « first ») and return.
-//
-// 5. If first does not contain any format specifiers, perform Printer(logLevel, args).
-//
-// 6.Otherwise, perform Printer(logLevel, Formatter(args)).
-//
-// 7. Return undefined.
+globalThis[internalSymbol]._printer = globalThis.printer;
 
-// Printer(logLevel, args[, options])
-// https://console.spec.whatwg.org/#printer
+globalThis[internalSymbol].printer = function printer(loglevel, args) {
+  if (args.length) {
+    args = args.join(' ');
+  }
+  if (loglevel === 'log') {
+    globalThis[internalSymbol]._printer(args)
+  } else if (loglevel === 'error') {
+    globalThis[internalSymbol]._printer(args, true)
+  }
+};
 
-globalThis.console.log = printer;
+globalThis[internalSymbol].logger = function logger(loglevel, args) {
+  if (!args.length) return;
+  let first = args[0];
+  let rest = args.slice(1);
+  if (!rest.length) {
+    globalThis[internalSymbol].printer(loglevel, first);
+    return;
+  }
+
+  // TODO Handle Format Specifiers
+
+  globalThis[internalSymbol].printer(loglevel, args);
+};
+
+globalThis.console.log = function log(...args) {
+  globalThis[internalSymbol].logger('log', args);
+};
+
+globalThis.console.error = function error(...args) {
+  globalThis[internalSymbol].logger('error', args);
+};
+
 globalThis.printer = undefined;
