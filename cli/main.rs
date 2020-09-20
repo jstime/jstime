2404d5aa595607740ead092bd130ce7c4a22ae8a
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-09-21 02:21:39 +0530

    WIP: Move generate_completions into Repl

--- cli/main.rs
+++ cli/main.rs
@@ -2,6 +2,7 @@ use dirs::home_dir;
 use jstime::JSTime;
 use jstime_core as jstime;
 use regex::Regex;
+use rustyline::Editor;
 use std::env;
 use std::path::PathBuf;
 use std::process;
@@ -68,66 +69,51 @@ fn main() {
             p
         });
 
-        Repl::new(jstime, history_file.unwrap()).readline();
+        Repl::new(&mut jstime, history_file.unwrap()).readline();
     }
 }
 
-// A rust implementation of the complete function from node repl.
-// https://github.com/nodejs/node/blob/c205f672e9cf0c70ea26f87eb97342947a244e18/lib/repl.js#L1136
-
-#[allow(dead_code)]
-fn generate_completions(line: &str, jstime: &mut JSTime) -> Vec<String> {
-    // <tab>: When pressed on a blank line, displays global and local (scope) variables.
-    // When pressed while entering other input, displays relevant autocompletion options.
-
-    // SIMPLE_EXPR_RE - Regex that splits input source into capture groups of Object property access
-    lazy_static! {
-        static ref SIMPLE_EXPR_RE: Regex =
-            Regex::new(r"(?m)(?:[a-zA-Z_$](?:\w|\$)*\??\.)*[a-zA-Z_$](?:\w|\$)*\??\.?$").unwrap();
-    };
-
-    // populate completion_groups based on the what the contextual completions for the line
-    let completion_groups: Vec<String> = vec![];
-
-    // return global builtins if the input line is empty.
-    if line.is_empty() {
-        let result = jstime.run_script(JS_GET_GLOBAL_BUILTINS, "jstime-repl");
-        let builtins = result.unwrap().split(',').map(|x| x.to_string()).collect();
-        builtins
-    } else {
-        match SIMPLE_EXPR_RE.captures(line) {
-            Some(x) => {
-                let complete_on = &x[0];
-                let mut expr = String::from("");
-
-                if line.ends_with(".") {
-                    // expr = complete_on.split()
-                } else {
-                    let bits: Vec<&str> = complete_on.split(".").collect();
-                    // filter = bits.pop().unwrap();
-                    expr = bits.join(".");
-                }
-
-                if expr.is_empty() {
-                } else {
-                    // based on the new node repl, we should probably parse the source into an ESTree
-                    // and then try to get the corresponding propertyNames?
-                }
-                completion_groups
-            }
-            _ => completion_groups,
-        }
-    }
-}
-
-#[allow(dead_code)]
-pub struct Repl {
-    jstime: JSTime,
+pub struct Repl<'a> {
+    jstime: &'a mut JSTime,
     history_file: PathBuf,
 }
 
-impl Repl {
-    pub fn new(jstime: JSTime, history_file: PathBuf) -> Self {
+// Rustyline seems to need an external struct to work properly.
+// It does not seem to like implementing the traits in the Repl struct itself.
+pub struct RustylineHelper {
+    completions: Vec<String>,
+}
+
+// Rustyline Helper requires a validator, hinter, highlighter and a completer
+// all of the implementations to the hinter trait can be empty
+impl rustyline::Helper for RustylineHelper {}
+impl rustyline::hint::Hinter for RustylineHelper {}
+impl rustyline::validate::Validator for RustylineHelper {}
+impl rustyline::highlight::Highlighter for RustylineHelper {}
+
+impl rustyline::completion::Completer for RustylineHelper {
+    // Candidate is an assosciated type, that helps the readlineHelper know the type of completion.
+    // Here the candidate refers to the "closest possible match" to a given string from all the completions / candidates available.
+    type Candidate = String;
+
+    fn complete(
+        &self,
+        _line: &str,
+        _pos: usize,
+        _ctx: &rustyline::Context<'_>,
+    ) -> std::result::Result<(usize, Vec<String>), rustyline::error::ReadlineError> {
+        let completions = self.completions.clone();
+        Ok((0, completions))
+    }
+
+    fn update(&self, line: &mut rustyline::line_buffer::LineBuffer, start: usize, elected: &str) {
+        let end = line.pos();
+        line.replace(start..end, elected)
+    }
+}
+
+impl<'a> Repl<'a> {
+    pub fn new(jstime: &'a mut JSTime, history_file: PathBuf) -> Self {
         let repl = Self {
             jstime,
             history_file,
@@ -135,56 +121,69 @@ impl Repl {
         repl
     }
 
-    pub fn readline(&mut self) {
-        use rustyline::{error::ReadlineError, Editor};
+    // A rust implementation of the complete function from node repl.
+    // https://github.com/nodejs/node/blob/c205f672e9cf0c70ea26f87eb97342947a244e18/lib/repl.js#L1136
+    pub fn generate_completions(&mut self, line: &str) -> Vec<String> {
+        // <tab>: When pressed on a blank line, displays global and local (scope) variables.
+        // When pressed while entering other input, displays relevant autocompletion options.
 
-        struct JstimeRustylineHelper {
-            // jstime: JSTime,
+        // SIMPLE_EXPR_RE - Regex that splits input source into capture groups of Object property access
+        lazy_static! {
+            static ref SIMPLE_EXPR_RE: Regex =
+                Regex::new(r"(?m)(?:[a-zA-Z_$](?:\w|\$)*\??\.)*[a-zA-Z_$](?:\w|\$)*\??\.?$")
+                    .unwrap();
         };
 
-        // Rustyline Helper requires a validator, hinter, highlighter and a completer
-        // all of the implementations to the hinter trait can be empty
-        impl rustyline::Helper for JstimeRustylineHelper {}
-        impl rustyline::hint::Hinter for JstimeRustylineHelper {}
-        impl rustyline::validate::Validator for JstimeRustylineHelper {}
-        impl rustyline::highlight::Highlighter for JstimeRustylineHelper {}
+        // populate completion_groups based on the what the contextual completions for the line
+        let completion_groups: Vec<String> = vec![];
 
-        impl rustyline::completion::Completer for JstimeRustylineHelper {
-            // Candidate is an assosciated type, that helps the readlineHelper know the type of completion.
-            // Here the candidate refers to the "closest possible match" to a given string from all the completions / candidates available.
-            type Candidate = String;
+        // return global builtins if the input line is empty.
+        if line.is_empty() {
+            let result = self
+                .jstime
+                .run_script(JS_GET_GLOBAL_BUILTINS, "jstime-repl");
+            let builtins = result.unwrap().split(',').map(|x| x.to_string()).collect();
+            builtins
+        } else {
+            match SIMPLE_EXPR_RE.captures(line) {
+                Some(x) => {
+                    let complete_on = &x[0];
+                    let mut expr = String::from("");
 
-            fn complete(
-                &self,
-                _line: &str,
-                _pos: usize,
-                _ctx: &rustyline::Context<'_>,
-            ) -> std::result::Result<(usize, Vec<String>), rustyline::error::ReadlineError>
-            {
-                // let completions = generate_completions(line, self.jstime);
-                let completions = vec![];
-                Ok((0, completions))
+                    if line.ends_with(".") {
+                        // expr = complete_on.split()
+                    } else {
+                        let bits: Vec<&str> = complete_on.split(".").collect();
+                        // filter = bits.pop().unwrap();
+                        expr = bits.join(".");
+                    }
+
+                    if expr.is_empty() {
+                    } else {
+                        // based on the new node repl, we should probably parse the source into an ESTree
+                        // and then try to get the corresponding propertyNames?
+                    }
+                }
+
+                None => {}
             }
-
-            fn update(
-                &self,
-                line: &mut rustyline::line_buffer::LineBuffer,
-                start: usize,
-                elected: &str,
-            ) {
-                let end = line.pos();
-                line.replace(start..end, elected)
-            }
+            completion_groups
         }
+    }
 
-        let mut rl = Editor::<JstimeRustylineHelper>::new();
+    pub fn readline(&mut self) {
+        use rustyline::error::ReadlineError;
 
-        // ERR: move occurs as `Copy` trait is not implemented.
-        let helper = JstimeRustylineHelper {
-            // jstime: self.jstime,
+        // This looks kinda weird, but Rustyline Editor requires a struct to be passed as Helper.
+        // Since Repl has already impl'd all the traits necessary for Helper we use that instead.
+
+        let helper = RustylineHelper {
+            completions: vec![],
         };
 
+        let mut rl = Editor::new();
         rl.set_helper(Some(helper));
+
         println!("Welcome to jstime v{}!", env!("CARGO_PKG_VERSION"));
 
         loop {
@@ -224,7 +223,7 @@ impl Repl {
 
 #[test]
 
-fn test_generate_completions() {
+fn test_global_completions() {
     jstime::init(Some(vec![]));
 
     let mut options = jstime::Options::default();
@@ -308,6 +307,12 @@ fn test_generate_completions() {
     .map(|x| x.to_string())
     .collect();
 
-    let completions = generate_completions("", &mut jstime);
+    let history_file = home_dir().map(|mut p| {
+        p.push(".jstime_repl_history");
+        p
+    });
+
+    let repl = &mut Repl::new(&mut jstime, history_file.unwrap());
+    let completions = repl.generate_completions("");
     assert_eq!(do_vecs_match(&completions, &globals), true);
 }
