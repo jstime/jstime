e807fa9fe5792c08d3dfc8f51af95fa6bb186b1c
Assaf Sapir <assapir@github.com>
2021-06-20 15:14:41 +0300

    Add integration tests

---
+++ cli/tests/fixtures/console-printer.js
@@ -0,0 +1,5 @@
+console.log('first', '%second', 'third');
+console.log('first%s', 'second', 'third');
+console.log('first %s', 'second', 'third');
+console.log('first %s %s', 'second', '3');
+console.log('first %s %s', 'second')
